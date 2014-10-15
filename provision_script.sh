a0759e8ad8df48a616b21c074784be08c9f9895a
David Cliff <d.cliff@neu.edu>
2014-10-15 11:02:33 -0400

    Unattended fixes, plus gitignore

--- provision_script.sh
+++ provision_script.sh
@@ -2,10 +2,13 @@
 
 /bin/bash --login
 
-sudo apt-get install tomcat7
+sudo apt-get update
+sudo apt-get install tomcat7 --yes
 
-sudo apt-get install python-software-properties
-sudo add-apt-repository ppa:webupd8team/java
+sudo apt-get install python-software-properties --yes
+sudo add-apt-repository ppa:webupd8team/java --yes
 sudo apt-get update
 
-sudo apt-get install oracle-java7-installer
+echo debconf shared/accepted-oracle-license-v1-1 seen true | sudo debconf-set-selections
+echo debconf shared/accepted-oracle-license-v1-1 select true | sudo debconf-set-selections
+sudo apt-get install oracle-java7-installer --yes
