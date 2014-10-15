3a8ee5d3947c08828e8673e55c371ed8885a2dbe
David Cliff <d.cliff@neu.edu>
2014-10-15 10:37:29 -0400

    First steps, tomcat and java

---
+++ provision_script.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+/bin/bash --login
+
+sudo apt-get install tomcat7
+
+sudo apt-get install python-software-properties
+sudo add-apt-repository ppa:webupd8team/java
+sudo apt-get update
+
+sudo apt-get install oracle-java7-installer
